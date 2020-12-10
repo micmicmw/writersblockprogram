9c68890632f0de0152a1e119935b37e81fe8fedb
Mic <micmicmw@gmail.com>
2020-12-10 13:27:04 -0700

    almost completed the U.I.
    Almost completed the UI. I am going to finishe that and create the prompt creator code later today.

---
+++ unity writers block program/Assets/scripts/worldcode.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class worldcode : MonoBehaviour
+{
+    public GameObject homepage;
+    public GameObject themespage;
+
+    public string detail = "empty";
+    
+    void Start()
+    {
+        homepage.active = true;
+        themespage.active = false;
+    }
+    public void themepageactivation()
+    {
+        homepage.active = false;
+        themespage.active = true;
+    }
+    public void homepageactivation()
+    {
+        homepage.active = true;
+        themespage.active = false;
+    }
+    public void detailSetbasic()
+    {
+        detail = "basic";
+    }
+    public void detailSetminor()
+    {
+        detail = "minor";
+    }
+    public void detailSetdetailed()
+    {
+        detail = "detailed";
+    }
+
+    public int detailFunctionselector()
+    {
+        if(detail == "basic")
+        {
+            return 1;
+        }else if(detail == "minor")
+        {
+            return 2;
+        }else if(detail == "detailed")
+        {
+            return 3;
+        }
+        else
+        {
+            Debug.Log("The detail was never set");
+        }
+        return 0;
+    }
+
+}
